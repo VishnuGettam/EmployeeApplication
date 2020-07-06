2a10e56d212cef8baf45b8e323e11480b6ba945f
VishnuGettam <vishnugettam@gmail.com>
2020-07-06 16:49:12 +0530

    added content and controller

---
+++ EmployeeStore/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeStore.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly List<Employee> employees;
+
+        public EmployeeController()
+        {
+            employees = new List<Employee>();
+            employees.Add(new Employee() { Id = 1, FirstName = "Vishnu", LastName = "Prasad", Gender = "Male" });
+        }
+
+        public ActionResult Index()
+        {
+            return View(employees);
+        }
+    }
+}
